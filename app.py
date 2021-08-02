ea4b0e573b5a5a4247a1683b7dffd8655e8cccf8
xnaud <tmdtp3143@gmail.com>
2021-08-02 21:52:07 +0900

    vue시작

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request, render_template  #request: 파라미터 값을 flask안에서 사용하기 위해
 app = Flask(__name__,static_url_path='/static') # static_url_path 옵션으로 static 폴더에 부트스트랩 적용에 필요한 파일들을 몰아 넣는다!
+                                                # brand / dist 같은 폴더는 애초 다운받은 부트스트랩에 저장된 폴더들이다. 
+                                                # 이 render_template 프레임워크(?)에서 중요한 것은 static이라는 폴더와 templates라는 폴더가 존재해야한다는 것이다.
 
 @app.route('/login')
 def login():
--- variable_html.py
+++ variable_html.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app=Flask(__name__)
 @app.route('/hello/<user>') # 변수값을 가져오기
 def hello_name(user):
-    return render_template('variable.html',name=user) # 폴더 경로!! html 파일이 꼭 template이라는 폴더 아래에 있어야 함
+    return render_template('variable.html',name1=user, name2 ='YOON' ) # 폴더 경로!! html 파일이 꼭 template이라는 폴더 아래에 있어야 함
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0',port='8082')
